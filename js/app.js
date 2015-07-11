32417476fb03b9c19b2cedd4be1967167e31f324
Fukuo Kadota <app.fk91@gmail.com>
2015-07-11 19:35:45 +0900

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var w = 900;
+var h = 900;
+
+var svg = d3.select("#map-body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+var color = d3.scale.category10();
+
+d3.json("../data/japan.topojson", function(json) {
+  var japan = topojson.object(json, json.objects.japan).geometries;
+  // 投影法設定
+  var projection = d3.geo.mercator()
+    .center([137, 34])
+    .translate([w/2, h/2])
+    .scale(1500);
+
+  // 緯度経度 ⇒ パスデータ変換設定
+  var path = d3.geo.path()
+    .projection(projection);
+
+  // パスデータとして日本地図描画
+  svg.selectAll("path")
+    .data(japan)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("stroke", "black")
+    .attr("stroke-width", 0.5)
+    .style("fill", function(d, i){ return color(i) });
+
+});
